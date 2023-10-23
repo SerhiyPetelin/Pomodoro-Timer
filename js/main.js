67d520f6975411fb6eaeb394a877de9f06cdc675
SerhiyPetelin <serhiypetelin@gmail.com>
2023-10-23 20:00:28 +0300

    Add start button logic.

--- js/main.js
+++ js/main.js
@@ -3,9 +3,40 @@
     const fehBody = document.body;
     const workDurationInput = document.getElementById('work-duration');
     const restDurationInput = document.getElementById('rest-duration');
-    const timerTime = document.getElementById('feh-tomer-time');
+    const timerTime = document.getElementById('feh-timer-time');
+
+    let workDuration = parseInt(workDurationInput.value) * 60;
+    let restDuration = parseInt(restDurationInput.value) * 60;
+    let remainingTime = workDuration;
+    let isPaused = true;
+    let isWorking = true;
+    let intervalId;
+
+    // Page loaded
 
     window.addEventListener('load', () => {
         fehBody.classList.add('page-loaded');
     });
+
+    // Start button
+    const startBtn = document.getElementById('start-btn');
+    startBtn.addEventListener('click', () => {
+        isPaused = false;
+
+        fehBody.classList.add('timer-running');
+
+        if(isWorking) {
+            fehBody.classList.remove('timer-paused');
+        }
+        else {
+            fehBody.classList.add('rest-mode');
+            fehBody.classList.remove('timer-paused');
+        }
+
+        if(intervalId) {
+            intervalId = setInterval(updateTimer, 1000)
+        }
+    });
+
+    // Update timer
 })();
