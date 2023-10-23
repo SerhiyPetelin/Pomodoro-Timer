2d5ec65780dc439a30bc99a73adba110006ff660
SerhiyPetelin <serhiypetelin@gmail.com>
2023-10-23 19:41:23 +0300

    Add styles to settings button and timer buttonns. Stated writind js logic.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 (function() {
 
     const fehBody = document.body;
-    
+    const workDurationInput = document.getElementById('work-duration');
+    const restDurationInput = document.getElementById('rest-duration');
+    const timerTime = document.getElementById('feh-tomer-time');
+
     window.addEventListener('load', () => {
         fehBody.classList.add('page-loaded');
     });
